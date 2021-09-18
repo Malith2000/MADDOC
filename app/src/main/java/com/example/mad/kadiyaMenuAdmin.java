1cb299c910a3353715337bcd7b66b80cdab35c12
Malith Senanayake <senanayakamalith@gmail.com>
2021-09-18 22:12:58 +0530

    Kadiya pages done

---
+++ app/src/main/java/com/example/mad/kadiyaMenuAdmin.java
@@ -0,0 +1,14 @@
+package com.example.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kadiyaMenuAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kadiya_menu_admin);
+    }
+}
