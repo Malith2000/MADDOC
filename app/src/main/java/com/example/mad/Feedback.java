a65195970c1cf772949b72da61c5f0730afad6dc
Hansaka2000 <hansakalakshan2000@gmail.com>
2021-09-23 10:26:01 +0530

    feedback update

--- app/src/main/java/com/example/mad/Feedback.java
+++ app/src/main/java/com/example/mad/Feedback.java
@@ -3,8 +3,15 @@ package com.example.mad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class Feedback extends AppCompatActivity {
+
+    EditText fist_name, last_name, phone_no, email, date, message;
+    RatingBar ratingBar;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
