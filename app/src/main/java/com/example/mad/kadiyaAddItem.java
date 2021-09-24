5264eae2abffc3e259155dc0d20f80f6c172c61e
Malith Senanayake <senanayakamalith@gmail.com>
2021-09-24 23:40:19 +0530

    Turning point

--- app/src/main/java/com/example/mad/kadiyaAddItem.java
+++ app/src/main/java/com/example/mad/kadiyaAddItem.java
@@ -3,12 +3,34 @@ package com.example.mad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class kadiyaAddItem extends AppCompatActivity {
+public class kadiyaAddItem extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kadiya_add_item);
+
+        Spinner spinner = findViewById(R.id.selectCategory);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
