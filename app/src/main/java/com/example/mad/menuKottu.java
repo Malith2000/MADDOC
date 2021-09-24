c8dcd959ca20fded90c5c561cb0a829ef9f39250
Malith Senanayake <senanayakamalith@gmail.com>
2021-09-24 18:26:30 +0530

    Dropdown cotd

---
+++ app/src/main/java/com/example/mad/menuKottu.java
@@ -0,0 +1,14 @@
+package com.example.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menuKottu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_kottu);
+    }
+}
